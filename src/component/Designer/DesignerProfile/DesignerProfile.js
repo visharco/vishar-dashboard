29c0efba58ebf85f0b0520254bb97ec112430ed4
moji0098 <darzimojtaba@gmail.com>
2019-04-22 10:46:48 +0430

    ACCOUNT

--- src/component/Designer/DesignerProfile/DesignerProfile.js
+++ src/component/Designer/DesignerProfile/DesignerProfile.js
@@ -67,11 +67,46 @@ class DesignerProfile extends Component {
                 <div className="DPE-desc-down" >
                     <div className="DPE-desc-box2" >
                         <div className="DPE-account" ref={this.accountBox}>
-                            <div className="DPE-desc-text" >
-                                <h1>توضیحات</h1>
-                                <p>
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-                                </p>
+                            <div className="DPEA-desc-text" >
+                                <div className="DPE-inputs" >
+                                    <Input
+                                        type={'text'}
+                                        name={'cardNumber'}
+                                        placeholder={'شماره کارت'}
+                                        changed={this.changedHandler}
+                                        error={this.state.forgetEmailError}
+                                    />
+                                    <div className="sample-card" >XXXX - XXXX - XXXX - XXXX</div>
+                                    <Input
+                                        type={'text'}
+                                        name={'nameFamily'}
+                                        placeholder={'نام و نام خانوادگی'}
+                                        changed={this.changedHandler}
+                                        error={this.state.forgetEmailError}
+                                    />
+                                    <Input
+                                        type={'text'}
+                                        name={'bandName'}
+                                        placeholder={' نام بانک'}
+                                        changed={this.changedHandler}
+                                        error={this.state.forgetEmailError}
+                                    />
+                                    
+                                    <div className="DPE-btns" >
+                                        <div className="DPE-cancel" >
+                                            انصراف
+                                        </div>
+                                        <Button
+                                            isLoading={this.state.isLoading}
+                                            title={'ذخیره'}
+                                            bgcolor={'#0080FF'}
+                                            hoverbgcolor={'#rgb(160, 160, 160)'}
+                                            click={this.callSubmit}
+                                            borderRadius="30px"
+                                            color="#fff"
+                                        />
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
