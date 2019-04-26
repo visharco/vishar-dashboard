c35d27f1121ba52d7a4fcba6e30bb3cbc5f6d131
aras armani <aras.armani2014@gmail.com>
2019-04-26 11:54:14 +0300

    readOnly is  added in input

--- src/component/profile/profile.js
+++ src/component/profile/profile.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 
 //
-//
+// Internal Component
 //
 
-import usergrey from '../../assets/icons/usergrey.svg';
 import Input from '../../component/common/input/Input';
 import Button from '../../component/common/Button/Button';
 
 //
-//compoents
+// ّIcons -------------------->
 //
+import usergrey from '../../assets/icons/usergrey.svg';
+
+//
+// Controler-------------------->
+//
+
+import GetToApi from '../../controler/getToApi';
+import Token from '../../api/token';
+
 
 
 import './style.css';
@@ -20,8 +28,35 @@ class Profile extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+
+        }
     }
+
+
+    changedHandler = (e) => { 
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    componentDidMount = async()=> {
+        const res = await GetToApi('profile/init');
+        console.log(res)
+        this.setState({
+            name : res.data.name,
+            email : res.data.email,
+            phone : res.data.phone,
+            tell : res.data.tell,
+            city : res.data.city
+        })
+
+
+
+        
+    }
+
+
 
     render() {
         return (
@@ -38,6 +73,7 @@ class Profile extends Component {
                             placeholder={'نام و نام خانوادگی'}
                             changed={this.changedHandler}
                             error={this.state.forgetEmailError}
+                            val={this.state.name}
                         />
                         <Input
                             type={'text'}
@@ -45,13 +81,16 @@ class Profile extends Component {
                             placeholder={'ایمیل'}
                             changed={this.changedHandler}
                             error={this.state.forgetEmailError}
+                            val={this.state.email}
+                            readonly={true} 
                         />
                         <Input
                             type={'text'}
                             name={'mobile'}
-                            placeholder={' موبایل'}
+                            placeholder={' تلفن همراه'}
                             changed={this.changedHandler}
                             error={this.state.forgetEmailError}
+                            val={this.state.phone}
                         />
                         <Input
                             type={'text'}
@@ -59,6 +98,7 @@ class Profile extends Component {
                             placeholder={'تلفن'}
                             changed={this.changedHandler}
                             error={this.state.forgetEmailError}
+                            val={this.state.tell}
                         />
                         <Input
                             type={'text'}
@@ -66,6 +106,7 @@ class Profile extends Component {
                             placeholder={'شهر / مکان'}
                             changed={this.changedHandler}
                             error={this.state.forgetEmailError}
+                            val={this.state.city}
                         />
                         <div className="PE-btns" >
                             <div className="PE-cancel" >
