1ecfbed0c9f6fe69855c91aa53e08dc722559603
yaser <yaser.darzi@gmail.com>
2019-05-24 18:10:06 +0430

    fix invoice

--- src/component/Designer/DesignerWallet/DesignerWallet.js
+++ src/component/Designer/DesignerWallet/DesignerWallet.js
@@ -1,17 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import GetToApi from '../../../controler/getToApi';
-
-//
-//
-//
-
+import PostToApi from '../../../controler/postToApi';
+import SweetAlert from 'sweetalert-react';
+import '../../../../node_modules/sweetalert/dist/sweetalert.css'
+import {browserHistory} from "react-router";
 import DesignerNoWallet from './../DesignerNoWallet/DesignerNoWallet'
 import Button from './../../common/Button/Button'
-//
-//compoents
-//
-
-
 import './style.css';
 import StatusMessage from '../../StatusMessage/StatusMessage';
 
@@ -21,68 +15,91 @@ class DesignerWallet extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            statusYellow:true,
-            data:[],
-            wallet:{}
+            statusYellow: true,
+            data: [],
+            wallet: {}
         }
     }
-    componentWillMount =async () => {
+
+    componentWillMount = async () => {
 
         const res = await GetToApi('wallet/invoice');
         console.log(res)
         this.setState({
-            data : res.data.invoice,
-            wallet:res.data.wallet
+            data: res.data.invoice,
+            wallet: res.data.wallet
         })
     }
 
+
+    requestWallet = async () => {
+        let data = new FormData();
+        const res = await PostToApi(data, 'wallet/request');
+        if (res.status === 200) {
+            this.setState({
+                alertText: "کاربر گرامی درخواست شما با موفقیت ارسال شد.",
+                show: true
+            });
+            browserHistory.push('/DesignerPayments');
+        } else
+            this.setState({
+                alertText: res.error,
+                show: true
+            });
+    };
 
 
     render() {
 
         const _renderInvoice = (
-            this.state.data ? this.state.data.map((data,index) => {
-                return        <tr className="DW-table-body" key={index}>
-                                <td className="DWT-child1" >{index+1}</td>
-                                <td className="DWT-child" >{data.price} تومان</td>
-                                {/* <td className="DWT-child" >{data.created_at}</td> */}
-                                <td className="DWT-child" >{data.created_at_persian}</td>
-                                <td className="DWT-child" >{data.invoice_status} </td>
-                                <td className={" DWT-child " + data.status  } >{data.status} </td>
-                            </tr>
-            }) :    <DesignerNoWallet />
+            this.state.data ? this.state.data.map((data, index) => {
+                return <tr className="DW-table-body" key={index}>
+                    <td className="DWT-child1">{index + 1}</td>
+                    <td className="DWT-child">{data.price} تومان</td>
+                    {/* <td className="DWT-child" >{data.created_at}</td> */}
+                    <td className="DWT-child">{data.created_at_persian}</td>
+                    <td className="DWT-child">{data.invoice_status} </td>
+                    <td className={" DWT-child " + data.status}>{data.status} </td>
+                </tr>
+            }) : <DesignerNoWallet/>
         )
 
 
         return (
 
             <div className="DesignerWallet">
-                <div className="DW-title" >
+                <SweetAlert
+                    show={this.state.show}
+                    title="اعلان"
+                    text={this.state.alertText}
+                    onConfirm={() => this.setState({show: false})}
+                />
+                <div className="DW-title">
                     کیف پول
-            </div>
+                </div>
 
-            <StatusMessage  
-                type="warning"
-                text='کاربر گرامی، شما زمانی می توانید از کیف پول خود برداشت کنید که موجودی شما بیش تر از 50،000 تومان باشد.' 
-            />
-                <div className="DW-table" >
-                    <table  >
+                <StatusMessage
+                    type="warning"
+                    text='کاربر گرامی، شما زمانی می توانید از کیف پول خود برداشت کنید که موجودی شما بیش تر از 50،000 تومان باشد.'
+                />
+                <div className="DW-table">
+                    <table>
                         <tbody>
-                            <tr className="DW-table-title">
-                                <th className="DWT-child1" >ردیف</th>
-                                <th className="DWT-child" >مبلغ </th>
-                                <th className="DWT-child" >تاریخ</th>
-                                <th className="DWT-child" >توضیحات</th>
-                                <th className="DWT-child" >وضعیت</th>
-                            </tr>
-                      
-                            {_renderInvoice}
+                        <tr className="DW-table-title">
+                            <th className="DWT-child1">ردیف</th>
+                            <th className="DWT-child">مبلغ</th>
+                            <th className="DWT-child">تاریخ</th>
+                            <th className="DWT-child">توضیحات</th>
+                            <th className="DWT-child">وضعیت</th>
+                        </tr>
+
+                        {_renderInvoice}
                         </tbody>
                     </table>
 
                 </div>
-                <div className="DW-request-box" >
-                    <div className="DW-money" >
+                <div className="DW-request-box">
+                    <div className="DW-money">
                         <span>موجودی کیف پول</span>
                         <span>{this.state.wallet.price} تومان</span>
                     </div>
@@ -91,16 +108,11 @@ class DesignerWallet extends Component {
                         title={'درخواست برای گرفتن پول'}
                         bgcolor={'#0090CF'}
                         hoverbgcolor={'#rgb(160, 160, 160)'}
-                        click={this.callSubmit}
+                        click={this.requestWallet}
                         borderRadius="30px"
                         color="#fff"
                     />
                 </div>
-
-
-
-                
-
 
 
             </div>
