baed3e7709e5c1fefa1a6db52856275a8b4eb111
aras armani <aras.armani2014@gmail.com>
2019-06-18 11:10:03 +0300

    fixed my profile is fixed

--- src/container/dashboard/dashboard.js
+++ src/container/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '../../component/common/Button/Button';
 import {browserHistory } from 'react-router'
+import GetApi from '../../controler/getToApi';
 
 
 
@@ -14,37 +15,96 @@ class DashboardComponent extends Component {
     }
 
 
+
+    componentDidMount = async () => {
+        const  res = await GetApi('profile/init');
+        console.log(res)
+        if(res.status === 200){
+            await this.setState({
+                type:res.data.type, 
+                name: res.data.name
+            })
+        } else if(res.status === 401) {
+
+            localStorage.removeItem('@authorization_vishar');   // remove local storage.
+            //  browserHistory.push('/login');      
+            window.location.reload();
+
+        }
+        
+
+
+    }
+
+
+
     callSubmit = () => {
         browserHistory.push('/createNewProject')
     }
 
+    callProject = () => {
+        browserHistory.push('/DesignerSingleProject')
+    }
+
 
     render() {
+
+        const renderCustomer = (
+            <div className="welcome-message">
+            <h2>
+                به حساب کاربری خود در ویشار خوش آمدید.
+            </h2>
+            <p>
+                جهت ثبت پروژه جدید، بر روی لینک زیر کلیک نمایید.
+            </p>
+            <Button                                                                  
+                isLoading={this.state.isLoading}                                    
+                title={'ایجاد پروژه جدید'}                                                      
+                bgcolor={'#0080FF'}                                                 
+                hoverbgcolor={'#0080FF'}                                          
+                click={this.callSubmit}
+                borderRadius="10px"
+                color="#fff"
+                id={'1'}
+                />  
+
+
+         </div>
+        )
+
+
+        const renderDesginer = (
+            <div className="welcome-message">
+            <h3>{this.state.name}</h3>
+            <h2>
+                به حساب کاربری خود در ویشار خوش آمدید.
+            </h2>
+            <p>
+                جهت مشاهده پروژه های موجود در ویشار، بر روی لینک زیر کلیک نمایید
+            </p>
+            <Button                                                                  
+                isLoading={this.state.isLoading}                                    
+                title={'مشاهده پروژه ها'}                                                      
+                bgcolor={'#0080FF'}                                                 
+                hoverbgcolor={'#0080FF'}                                          
+                click={this.callProject}
+                borderRadius="10px"
+                color="#fff"
+                id={'1'}
+                />  
+
+
+         </div>
+        )
+
+
         return (
             <div className="SingleProject">
                 <div className="SP-title" >
                     داشبورد
                 </div>
-                     <div className="welcome-message">
-                        <h2>
-                            به حساب کاربری خود در ویشار خوش آمدید.
-                        </h2>
-                        <p>
-                            جهت ثبت پروژه جدید، بر روی لینک زیر کلیک نمایید.
-                        </p>
-                        <Button                                                                  
-                            isLoading={this.state.isLoading}                                    
-                            title={'ایجاد پروژه جدید'}                                                      
-                            bgcolor={'#0080FF'}                                                 
-                            hoverbgcolor={'#1fc056cc'}                                          
-                            click={this.callSubmit}
-                            borderRadius="10px"
-                            color="#fff"
-                            id={'1'}
-                            />  
-
-
-                     </div>
+                {this.state.type === 'customer' ? renderCustomer :renderDesginer}
+              
 
 
                
