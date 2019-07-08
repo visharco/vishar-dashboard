61aab43f75a4ec369963cbff2c88415bb8364268
aras armani <aras.armani2014@gmail.com>
2019-07-08 07:51:08 +0300

    fixed all

--- src/component/project/project.js
+++ src/component/project/project.js
@@ -17,6 +17,16 @@ class Project extends Component {
         browserHistory.push('/projectDetail/' + id);
         // browserHistory.push({ pathname: '/projectDetail', state: this.props.id });
     }
+
+    getStatus = (key) => { 
+        switch (key) {
+            case 'active':
+                return 'تایید شده' 
+    
+            default:
+                return null 
+        }
+    }
     render() {
         return (
             <div key={this.props.id} className="project" onClick={() => this.goToProjectDetail(this.props.data.id)} >
@@ -30,7 +40,7 @@ class Project extends Component {
                         <div className="p-right-text" >
                             <div className="p-status-box" >
                                 <h1>{this.props.data.title}</h1>
-                                <p className={this.props.data.status} >{this.props.data.status}</p>
+                                <p className={this.props.data.status} >{this.getStatus(this.props.data.status)}</p>
                             </div>
                             <p>
                                 {/* لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است */}
